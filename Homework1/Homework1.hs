c9502be7e5ae8032bcda2142f874939ecd46801c
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-14 18:32:33 +0100

    Update Homework1.hs

--- Homework1/Homework1.hs
+++ Homework1/Homework1.hs
@@ -1,1 +1,9 @@
-module Homework1 where
+--Exercise 1--
+toDigits :: Integer -> [Integer]
+toDigits x  | x > 0  = toDigits (x`div` 10) ++ [x `mod`10]
+            | otherwise = [] 
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x = reverse (toDigits x)
+
+

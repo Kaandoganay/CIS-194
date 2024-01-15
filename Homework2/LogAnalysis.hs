8361b9182072823830da38950fb4974c805bb877
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-15 22:54:22 +0100

    sum

--- Homework2/LogAnalysis.hs
+++ Homework2/LogAnalysis.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
+import Text.XHtml (treeColors)
 
 --Exercise1--
 parseMessage :: String -> LogMessage
@@ -10,3 +11,16 @@ parseMessage string = case words string of
      ("W":sy : msg) -> LogMessage Warning (read sy) (unwords msg)
      (_) -> Unknown "This is not in the right format"
 
+
+parse :: String -> [LogMessage]
+parse string = map parseMessage  (lines string)
+
+--Exercise2--
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg@(LogMessage _ _ _) Leaf = Node Leaf msg Leaf
+insert lmsg1@(LogMessage _ ts1 _) (Node left lmsg2@(LogMessage _ ts2 _) right)
+  | ts1 > ts2 = Node left lmsg2 (insert lmsg1 right)
+  | otherwise = Node (insert lmsg1 left) lmsg2 right
+insert _ tree = tree
+
+--Exercise3--

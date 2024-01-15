3654ce6a0dc4f34446d55f9dd70915f0a6e3a154
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-15 05:58:05 +0100

    Update Homework1.hs

--- Homework1/Homework1.hs
+++ Homework1/Homework1.hs
@@ -31,5 +31,4 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n a c b
   | n <= 0    = []
-  | otherwise = hanoi (n - 1) a b c ++
-      ((a, c) : hanoi (n - 1) b c a)
+  | otherwise = hanoi (n - 1) a b c ++ ((a, c) : hanoi (n - 1) b c a)

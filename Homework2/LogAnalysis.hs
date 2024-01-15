15df4edf0cfbeb4d1c2f1eaea24ccf67b942dfa5
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-15 19:33:24 +0100

    new

--- Homework2/LogAnalysis.hs
+++ Homework2/LogAnalysis.hs
@@ -1,2 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
+import Log
+
+--Exercise1--
+parseMessage :: String -> LogMessage
+parseMessage string = case words string of 
+     ("E":st: sy : msg) -> LogMessage (Error (read st))  (read sy) (unwords msg)
+     ("I":sy :msg) -> LogMessage Info (read sy) (unwords msg)
+     ("W":sy : msg) -> LogMessage Warning (read sy) (unwords msg)
+     (_) -> Unknown "This is not in the right format"
 

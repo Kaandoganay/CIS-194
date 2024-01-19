abb0f57bdf16f09a9fb53409115dbcc63d32b087
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-19 21:38:58 +0100

    Update Golf.hs

--- Homework3/Golf.hs
+++ Homework3/Golf.hs
@@ -1,5 +1,11 @@
 module Golf where
 --Exercise 1 Hopscotch--
+taken :: Int -> [a] -> [a]
+taken n xs = [xs !! k | k <- [n-1, n-1+n..length xs - 1]]
 
 skips :: [a] -> [[a]]
-skips xs= map (drop 3) (replicate (length xs) xs) 
+skips xs = [taken n xs | n <- [1..length xs ]  ]
+
+--exercise2--
+
+localMaxima xs = [n | n <-xs,   ]

f6108337baf5fd23439309805e3bd6d73c84a72a
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-19 22:30:07 +0100

    Update Golf.hs

--- Homework3/Golf.hs
+++ Homework3/Golf.hs
@@ -1,4 +1,5 @@
 module Golf where
+
 --Exercise 1 Hopscotch--
 taken :: Int -> [a] -> [a]
 taken n xs = [xs !! k | k <- [n-1, n-1+n..length xs - 1]]
@@ -7,5 +8,8 @@ skips :: [a] -> [[a]]
 skips xs = [taken n xs | n <- [1..length xs ]  ]
 
 --exercise2--
-
-localMaxima xs = [n | n <-xs,   ]
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs)
+  | x < y && y > z = y : localMaxima (y:z:xs)
+  | otherwise      = localMaxima (y:z:xs)
+localMaxima _ = []

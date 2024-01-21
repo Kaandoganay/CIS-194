31ddce7e0b7bc62a51d1fa1fd966e8836a4b22c1
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-21 11:56:17 +0100

    Update Golf.hs

--- Homework3/Golf.hs
+++ Homework3/Golf.hs
@@ -13,3 +13,16 @@ localMaxima (x:y:z:xs)
   | x < y && y > z = y : localMaxima (y:z:xs)
   | otherwise      = localMaxima (y:z:xs)
 localMaxima _ = []
+
+--exercise3--
+
+histogram :: [Integer] -> String
+histogram xs = unlines (map (stars a) [k+1,k..1]) ++ "==========\n0123456789\n"
+     where a = count xs
+           k = maximum a
+
+stars :: [Int] -> Int -> String
+stars xs n = [if i >= n then '*' else ' ' | i <- xs]
+
+count :: [Integer] -> [Int]
+count xs = map (\n -> length $ filter (== n) xs) [0..9]

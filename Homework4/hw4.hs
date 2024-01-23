7961687522daef76fadc5f58012e78aaa1fd13b0
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-23 14:09:06 +0100

    Update hw4.hs

--- Homework4/hw4.hs
+++ Homework4/hw4.hs
@@ -8,4 +8,10 @@
 fun1' :: [Integer] -> Integer
 fun1'= product . map (\x -> x-2) . filter even
 
---
+--fun2 :: Integer -> Integer
+--fun2 1 = 0
+--fun2 n | even n = n + fun2 (n ‘div‘ 2)
+-- | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2'= sum . takeWhile (/=1) . iterate (\x -> if even x then x `div` 2 else 3*x+1)

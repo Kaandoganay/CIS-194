6ed96cc8a852d8b257b6a49feb9242f95bcab98a
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-24 23:17:28 +0100

    new

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -1,4 +1,12 @@
 module Calc where
-    import ExprT ()
+
+import ExprT
+import Parser
+import StackVM
+
 
 --Exercise1--
+eval :: ExprT -> Integer
+eval (ExprT.Lit x) = x
+eval (ExprT.Add x y) = eval x  + eval y
+eval (ExprT.Mul x y) = eval x * eval y

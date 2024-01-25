d75b7456dde8d8926c2a18e307dc0b73b67a5398
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-25 19:21:22 +0100

    Update Calc.hs

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -3,7 +3,8 @@ module Calc where
 import ExprT
 import Parser
 import StackVM
-
+import Data.Maybe
+import qualified Data.Map
 
 --Exercise1--
 eval :: ExprT -> Integer
@@ -12,3 +13,6 @@ eval (ExprT.Add x y) = eval x  + eval y
 eval (ExprT.Mul x y) = eval x * eval y
 
 --Exercise2--
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp ExprT.Lit ExprT.Add ExprT.Mul

65446cf8c26f81e73134a23c34f83d57173d46ab
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-01-27 14:45:37 +0100

    Update Calc.hs

--- Homework5/Calc.hs
+++ Homework5/Calc.hs
@@ -30,5 +30,30 @@ instance Expr ExprT where
     mul = ExprT.Mul
 
 --Exercise4--
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (<= 0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+
+instance Expr MinMax where
+   
+   add (MinMax x) (MinMax y) = MinMax $ max x y
+   mul (MinMax x) (MinMax y) = MinMax $ min x y
+   lit                       = MinMax
+
+instance Expr Mod7 where
+    add (Mod7 x) (Mod7 y) = Mod7 $ (x+y) `mod` 7
+    mul (Mod7 x) (Mod7 y) = Mod7 $ (x*y) `mod` 7
+    lit   x               = Mod7 $ x `mod` 7
+
 
 
